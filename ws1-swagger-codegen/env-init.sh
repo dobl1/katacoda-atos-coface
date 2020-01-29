ce0bd42e6d1cc941f5b480bad3ee192a74a9f1fd
Dorian Blanc <dorian.blanc@atos.net>
2020-01-29 22:38:07 +0100

    use 3.0.10

--- ws1-swagger-codegen/env-init.sh
+++ ws1-swagger-codegen/env-init.sh
@@ -1,2 +1,2 @@
 apt install -y httpie
-wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.16/swagger-codegen-cli-3.0.16.jar -O swagger-codegen-cli.jar
+wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.16/swagger-codegen-cli-3.0.10.jar -O swagger-codegen-cli.jar
